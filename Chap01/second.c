3a33c94cdb6964ae924a185ef3cec0c73b2faf19
rosa_ <rosa_@HOME>
2025-04-01 21:16:20 +0900

    .

---
+++ Chap01/second.c
@@ -0,0 +1,22 @@
+/* 파일명: second.c
+
+ * 작성자: 박로사
+
+ * 날짜: 2025.3.17
+
+ * 버전: 17.13.3
+
+ */
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	printf("10 + 20 = %d\n", 10 + 20);
+	printf("56 - 32 = %d\n", 56 - 32);
+	printf("11 * 31 = %d\n", 11 * 31);
+	printf("72 / 12 = %d\n", 72 / 12);
+
+	return 0;
+}
---
+++ Chap01/test.c
@@ -0,0 +1,19 @@
+/* 파일명: test.c
+
+ * 작성자: 박로사
+
+ * 날짜: 2025.3.17
+
+ * 버전: 17.13.3
+
+ */
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello, World!\n");
+
+	return 0;
+}
