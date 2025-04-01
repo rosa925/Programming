2e18d188c1e776f7f633ad649c2c72180f2890d6
rosa_ <rosa_@HOME>
2025-04-01 21:14:53 +0900

    .

---
+++ Chap01/first.c
@@ -0,0 +1,21 @@
+/* 파일명: first.c
+
+ * 작성자: 박로사
+
+ * 날짜: 2025.3.17
+
+ * 버전: 17.13.3
+
+ */
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	printf("FirstApp 프로그램\n");
+	printf("작성자: 천정아\n");
+	printf("버전: 1.0\n");
+
+	return 0;
+}
